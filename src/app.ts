7275acf38d314324e6e2a175980dc5ea99ff56bf
ShashankJ017 <SJangir@medisticshealth.com>
2026-04-16 16:54:01 +0530

    added deployed origin url

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://tristate-frontend.onrender.com/",
     credentials: true,
   }),
 );
