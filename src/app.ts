a2cb755cd79b519e0088984cca809cca9df31c23
Prince Akabari <Pakabari@medisticshealth.com>
2026-04-16 17:03:37 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://tristate-frontend.onrender.com/",
+    origin: "https://tristate-frontend.onrender.com",
     credentials: true,
   }),
 );
