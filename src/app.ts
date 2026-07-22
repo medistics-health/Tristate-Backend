90a9cdc98193400e81941d933629448892bac702
pakabari84 <pakabari@medisticshealth.com>
2026-07-22 11:18:07 +0530

    changes

--- src/app.ts
+++ src/app.ts
@@ -41,8 +41,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use("/api/v1/stripe/webhook", stripeWebhookRouter);
-app.use(express.json({ limit: "25mb" }));
-app.use(express.urlencoded({ extended: true, limit: "25mb" }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(cookieParser());
 app.use(
   cors({
